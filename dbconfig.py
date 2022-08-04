272250d953fa287d8478665d0e0af174e38ba482
sosim <Vlad-1981>
2022-08-04 09:27:05 +0300

    Added files for connecting with MySQL / SQLite3 db

---
+++ dbconfig.py
@@ -0,0 +1,23 @@
+
+dbconfig = {                                            #   подключение к MySQL
+                'host': 'localhost',
+                'user': 'root',
+                'password': '---',                      #   enter your password
+                'port': 3306,
+                'database': 'currencys',
+}
+
+dbconfig_2 = {                                          #   подключение к MySQL
+                'host': 'localhost',
+                'user': 'root',
+                'password': '---',                          #   enter your password
+                'port': 3306,
+}
+
+extra_options_db = {
+                    'table_name': 'exchange_rates',
+                    'new_database': 'currencys',
+}
+
+
+
